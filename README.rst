ca50045dccb31612514cb0be09c03b3de3488eef
Jorge R. Herskovic <jherskovic@gmail.com>
2019-07-26 15:59:48 -0500

    First working release. No tests, sorry.

---
+++ README.rst
@@ -0,0 +1,49 @@
+SQLAlchemy-postgresxl
+=====================
+
+Jorge R. Herskovic <jherskovic@gmail.com>
+
+A Postgres-XL SQLAlchemy dialect (inherits its functionality from the built-in Postgres dialect,
+so postgres applications should work out of the box)
+
+It accepts two different drivers:
+
+- ``psycopg2``
+- ``psycopg2cffi``
+
+and it adds two table-level options:
+
+- ``postgresxl_distribute_by``: ``[hash|modulo|replication|roundrobin]``
+- ``postgresxl_distribute_column``: [name of the column to hash or modulo on; required for those two distribution mode]
+
+``postgresxl_distribute_column`` is ignored if the ``distribute_by`` option is ``replication`` or ``roundrobin``.
+
+That's pretty much it.
+
+Example, using declarative:
+
+::
+
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy.ext.declarative import declarative_base
+
+    Base = declarative_base()
+
+    class SomeClass(Base):
+        __tablename__ = 'some_table'
+        __table_args__ = {
+            'postgresxl_distribute_by': 'hash',
+            'postgresxl_distribute_column': 'id',
+        }
+        id = Column(Integer, primary_key=True)
+        name =  Column(String(50))
+
+
+    class SomeClass2(Base):
+        __tablename__ = 'some_other_table'
+        __table_args__ = {
+            'postgresxl_distribute_by': 'replication',
+        }
+        id = Column(Integer, primary_key=True)
+        name =  Column(String(50))
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import re
+
+from setuptools import setup
+
+v = open(os.path.join(os.path.dirname(__file__), 'sqlalchemy_postgresxl', '__init__.py'))
+VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
+v.close()
+
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+
+setup(name='sqlalchemy_postgresxl',
+      version=VERSION,
+      description="Postgresql-XL for SQLAlchemy",
+      long_description=open(readme).read(),
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: Implementation :: CPython',
+      'Topic :: Database :: Front-Ends',
+      ],
+      keywords='SQLAlchemy Postgres-XL',
+      author='Jorge R. Herskovic',
+      author_email='jherskovic@gmail.com',
+      license='MIT',
+      packages=['sqlalchemy_postgresxl'],
+      include_package_data=True,
+      tests_require=['nose >= 0.11'],
+      test_suite="nose.collector",
+      zip_safe=False,
+      entry_points={
+         'sqlalchemy.dialects': [
+              'postgresxl = sqlalchemy_postgresxl.psycopg2:Pgxl_Dialect_psycopg2',
+              'postgresxl.psycopg2 = sqlalchemy_postgresxl.psycopg2:Pgxl_Dialect_psycopg2',
+              'postgresxl.psycopg2cffi = sqlalchemy_postgresxl.psycopg2cffi:Pgxl_Dialect_psycopg2cffi', # For PyPy
+         ]
+        }
+)
