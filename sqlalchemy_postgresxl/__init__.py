d766873a8036cf58a44b7ee494c87dc4ad6b6b54
Jorge R. Herskovic <jherskovic@gmail.com>
2019-07-26 16:11:34 -0500

    First working release. Added copyright text and license.

--- sqlalchemy_postgresxl/__init__.py
+++ sqlalchemy_postgresxl/__init__.py
@@ -1,3 +1,12 @@
+# sqlalchemy_postgresxl/__init__.py
+# Copyright (C) 2019 Jorge R. Herskovic <jherskovic@gmail.com>
+# This work is based in part on the SQLAlchemy postgres dialect code written by the SQLAlchemy authors.
+#
+# Some parts are based on the Access adapter for SQLAlchemy by Mike Bayer, available at
+# https://bitbucket.org/zzzeek/sqlalchemy-access/src/default/
+# This module is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
 __version__ = '0.1'
 
 from sqlalchemy.dialects import registry
